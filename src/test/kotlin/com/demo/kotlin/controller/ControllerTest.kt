44fbfb76fc5db2284ffe431072f749f614d9e346
Marina <marina.jarvinen@hotmail.com>
2024-01-27 10:47:28 +0200

    root controller & test

---
+++ src/test/kotlin/com/demo/kotlin/controller/ControllerTest.kt
@@ -0,0 +1,24 @@
+package com.demo.kotlin.controller
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ControllerTest() {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return hello message with base URL`() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api"))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.content().string("Hello"))
+    }
+}
